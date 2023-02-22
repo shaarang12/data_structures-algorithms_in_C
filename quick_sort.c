037f0b437bc0aa029aaaa73fbf1db839abdfa460
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-02-22 12:56:57 +0530

    Quick Sort

---
+++ quick_sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void quickSort(int arr[], int low, int high){
+    if(low >= high){
+        return;
+    }
+    int start = low;
+    int end =  high;
+    int mid = start + (end - start) / 2;
+    int pivot = arr[mid];
+    while(start <= end){
+        while(arr[start] < pivot)
+            start ++;
+        while(arr[end] > pivot)
+            end --;
+        if(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start ++;
+            end --;
+        }
+    }
+    quickSort(arr, low, end);
+    quickSort(arr, start, high);
+}
+
+int main(){
+    int arr[] = {5,4,-1, -8, 3, 2, 1};
+    quickSort(arr, 0, sizeof(arr)/4 - 1);
+    for(int i = 0; i < sizeof(arr) / 4; i ++){
+        printf("%d ", arr[i]);
+    }
+}
